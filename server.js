b252cf7793f23cd99c79d67afbcc6844a4f08bf6
wissem slim <wissem.slim@omegup.tn>
2024-08-02 18:16:58 +0200

    initiate

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const xlsx = require("xlsx");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Function to read Excel file and convert to JSON
+const readExcelFile = (filePath) => {
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const headers = xlsx.utils.sheet_to_json(worksheet, {
+    header: 1,
+  });
+  const jsonData = xlsx.utils.sheet_to_json(worksheet);
+  return jsonData;
+};
+const filePath = path.join(__dirname, "FMSCA_records.xlsx");
+let data = [];
+if (fs.existsSync(filePath)) {
+  data = readExcelFile(filePath);
+}
+// Endpoint to get Excel data
+app.get("/excel-data", (req, res) => {
+  if (fs.existsSync(filePath)) {
+    res.json(data);
+  } else {
+    res.status(404).send("File not found");
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
